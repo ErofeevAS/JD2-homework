88d86d28f71ecb2ef002e751dda04c394f5773be
ErofeevAS <spokeman152@gmail.com>
2019-04-06 13:05:26 +0300

    empty project

---
+++ firstweek/web-module/src/main/java/com/gmail/erofeev/st/alexei/controller/App.java
@@ -0,0 +1,13 @@
+package com.gmail.erofeev.st.alexei.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
